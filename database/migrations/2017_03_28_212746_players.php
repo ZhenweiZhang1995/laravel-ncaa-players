f5832da9795a87ad22cc2d5d11fb95e752cf4459
ZhenweiZhang1995 <apple@dhcp-v880-10-00493.1Xwireless.unc.edu>
2017-03-28 17:41:28 -0400

    finish exercise

--- database/migrations/2017_03_28_212746_players.php
+++ database/migrations/2017_03_28_212746_players.php
@@ -13,6 +13,23 @@ class Players extends Migration
      */
     public function up()
     {
+      Schema:: create('games',function (Blueprint $table){
+        $table->increments('id');
+        $table->string('first')->default('');
+        $table->string('last')->default('');
+        $table->integer('gamesPlayed')->default(0);
+        $table->integer('avgMinutes')->default(0);
+        $table->integer('ppg')->default(0);
+        $table->integer('rpg')->default(0);
+        $table->integer('apg')->default(0);
+        $table->integer('spg')->default(0);
+        $table->integer('bpg')->default(0);
+        $table->integer('tpg')->default(0);
+        $table->float('fgPercent',0,2)->default(0.00);
+        $table->float('ftPercent',0,2)->default(0.00);
+        $table->float('tpPercent',0,2)->default(0.00);
+        $table->timestamps();
+      });
         //
     }
 
@@ -23,6 +40,7 @@ class Players extends Migration
      */
     public function down()
     {
+      Schema::dropIfExists('players');
         //
     }
 }
